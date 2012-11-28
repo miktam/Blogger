ace6f384b2015242eb3978951aa1fe2a0ecfa0f4
Andrei Karpushonak <karpushonak@gmail.com>
2012-11-28 18:45:03 +0100

    added first mocha tests

--- test/test-blog.js
+++ test/test-blog.js
@@ -1,10 +1,42 @@
-var assert = require("assert")
+var assert = require('assert')
+var app = require('../app');
+var should = require('should');
+var request = require('request');
+var assert = require('assert');
 
-	describe('Array', function(){
-			describe('#indexOf()', function(){
-				it('should return -1 when the value is not present', function(){
-					assert.equal(-1, [1,2,3].indexOf(5));
-					assert.equal(-1, [1,2,3].indexOf(0));
-					})
-				})
-})
+describe('test if server is running', function(){
+	describe('GET /', function(){
+		it("should respond with status 200", function(done){
+			request('http://127.0.0.1:3000/', function(err,resp,body){
+				assert.equal(resp.statusCode, 200);
+				done(); 
+				}); 
+			}); 
+		});
+
+});
+
+describe('list all posts', function(){
+	describe('GET /posts', function(){
+		it("should respond with status 200", function(done){
+			request('http://127.0.0.1:3000/', function(err,resp,body){
+				assert.equal(resp.statusCode, 200);
+				done(); 
+				}); 
+			}); 
+		});
+
+});
+
+describe('another list all posts', function(){
+	describe('GET /posts', function(){
+		it("should respond with status 200", function(done){
+			request('http://127.0.0.1:3000/', function(err,resp,body){
+				assert.equal(resp.statusCode, 200);
+				console.log("body:" + body);
+				done(); 
+				}); 
+			}); 
+		});
+
+});
