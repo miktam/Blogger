2d938fea902959bd50bc769dd9e1fd533937eeaf
Andrei Karpushonak <karpushonak@gmail.com>
2012-11-30 07:11:47 +0100

    Implemented getting post along with its comments, added sunny/rainy day scenarios.

--- routes/routes.js
+++ routes/routes.js
@@ -6,6 +6,10 @@ var postSchema = "storage:posts"
 /**
  * Store post with title as a key in hash (replace " " to "-" first)
  * Stringify whole post object (title, message, and add created date) to value
+ *
+ * @param post.title 	title of the post
+ * @param post.message	body of the post
+ * @return post 		created post
  */
 exports.createBlogPost = function(req, res) {
 
@@ -15,7 +19,6 @@ exports.createBlogPost = function(req, res) {
 		var post = {
 			title: title,
 			message: message,
-			createdAt: new Date()
 		}
 		var id = post.title.replace(" ", "-")
 		client.hset(postSchema, id, JSON.stringify(post))
@@ -28,7 +31,7 @@ exports.createBlogPost = function(req, res) {
 
 /**
  * Get all posts
- * Query for all keys first, then extract values, and populate res with extracted data
+ * @return list of all existing posts
  */
 exports.findAllPosts = function(req, res) {
 	var posts = []
@@ -46,12 +49,42 @@ exports.findAllPosts = function(req, res) {
 };
 
 
+/**
+ * Find blog post by id
+ * If comments are linked to this post, include comments as well
+ *
+ * @param get.id 	id of the post
+ * @return post 	found post with comment if exist
+ *		  			410 otherwise
+ */
 exports.findBlogById = function(req, res) {
 
-	res.send({
-		id: req.params.id,
-		title: 'First post for get',
-		body: 'Not much as the body of the blog post'
+	var id = req.params.id.replace(" ", "-")
+	client.hget(postSchema, id, function(err, result) {
+
+		if(result != undefined) {
+			var entry = JSON.parse(result);
+			var post = {
+				title: entry.title,
+				message: entry.message,
+			}
+
+			// check if there is connected comments for that
+			client.smembers(id, function(err, comments) {
+
+				if(result != undefined) {
+					var comments = JSON.parse(comments);
+				} // else there is no comments for this post
+
+				res.send({
+					post: post,
+					comments: comments
+				});
+			});
+
+		} else {
+			res.send(410)
+		}
 	});
 
 };
@@ -59,11 +92,11 @@ exports.findBlogById = function(req, res) {
 /**
  * Update blog post
  *
- * @param get.id it of post to update
- * @param post.title new title to insert
- * @param post.message new message to insert
- * @return updated message if found
- *		   410 otherwise
+ * @param get.id 		id of the post to update
+ * @param post.title 	new title to insert
+ * @param post.message 	new message to insert
+ * @return post 		updated post if found
+ *		   				410 otherwise
  */
 exports.updateBlogPost = function(req, res) {
 
@@ -78,7 +111,6 @@ exports.updateBlogPost = function(req, res) {
 			var post = {
 				title: title,
 				message: message,
-				createdAt: new Date()
 			}
 
 			client.hset(postSchema, id, JSON.stringify(post))
@@ -97,9 +129,9 @@ exports.updateBlogPost = function(req, res) {
 /**
  * Delete blog post
  *
- * @param get.id it of post to delete
- * @return 200 if post was deleted
- *		   410 post not found
+ * @param get.id 	id of the post to delete
+ * @return status	200 if post was deleted
+ *		   			410 post not found
  */
 exports.deleteBlogPost = function(req, res) {
 
@@ -121,11 +153,11 @@ exports.deleteBlogPost = function(req, res) {
  * Comments are stored in SET, key for the set equals title of the post
  * If author and comment body already exist for given post, they don't be duplicated as it is SET
  *
- * @param get.id post title to add comment to
+ * @param get.id 		post title to add comment to
  * @param post.author	author of the comment
  * @param post.comment 	comment body
- * @return 	added comment if post exists
- *			410 otherwise
+ * @return comment 		added comment if post exists
+ *						410 otherwise
  */
 exports.createCommentForPost = function(req, res) {
 
@@ -159,11 +191,11 @@ exports.createCommentForPost = function(req, res) {
  * Comment is deleted when author and comment body matches
  * If post does not exist, skip deleting
  *
- * @param get.id post title to delete comment from
+ * @param get.id 		post title to delete comment from
  * @param post.author	author of the comment to delete
  * @param post.comment 	comment body to delete
- * @return 	added comment if post exists
- *			410 otherwise
+ * @return comment		added comment if post exists
+ *						410 otherwise
  */
 exports.deleteCommentForPost = function(req, res) {
 
--- test/test-blog.js
+++ test/test-blog.js
@@ -37,6 +37,35 @@ describe('create a blog post', function() {
 	})
 })
 
+var commentAuthor = 'Andrei'
+var commentBody = 'Will it scale?'
+
+describe('create a comment for a blog post', function() {
+	it('should return created comment', function(done) {
+		request({
+			method: 'POST',
+			url: url + '/blog/comment/create/' + blogPostTitleToCreate,
+			json: true,
+			body: {
+				author: commentAuthor,
+				comment: commentBody
+			}
+		}, function(err, res, body) {
+			if(err) {
+				done(err)
+			} else {
+				res.statusCode.should.be.equal(200)
+				body.should.be.a('object')
+				body.should.have.property('author')
+				body.should.have.property('comment')
+				assert.equal(body.author, commentAuthor, 'returned author shall be the same as submitted')
+				assert.equal(body.comment, commentBody, 'returned message shall be the same as submitted')
+				done()
+			}
+		})
+	})
+})
+
 describe('create second blog post', function() {
 	it('should return created blog post', function(done) {
 		request({
@@ -64,31 +93,29 @@ describe('create second blog post', function() {
 })
 
 describe('list all posts', function() {
-	it("GET /blog/posts should respond with list of blog posts", function(done) {
+	it("should return all created posts", function(done) {
 		request(url + '/blog/posts', function(err, resp, body) {
-			assert.equal(resp.statusCode, 200);
-			should.exist(body);
-			var posts = JSON.parse(body);
-			assert.equal(2, posts.length, "len shall be 2");
-			var post = posts[0];
-			assert.equal(post.title, blogPostTitleSecondToCreate, "title does not match");
-			done();
+			assert.equal(resp.statusCode, 200)
+			should.exist(body)
+			var posts = JSON.parse(body)
+			assert.equal(2, posts.length, "len shall be 2")
+			var post = posts[0]
+			assert.equal(post.title, blogPostTitleSecondToCreate, "title does not match")
+			done()
 		});
 	});
 
 });
 
-var blogPostIdToGet = 20;
 describe('show a post', function() {
-	it("GET /blog/post/:id should show a post", function(done) {
-		request(url + '/blog/post/' + blogPostIdToGet, function(err, resp, body) {
-			assert.equal(resp.statusCode, 200);
-			var obj = JSON.parse(body);
-
-			obj.should.be.a('object');
-			obj.should.have.property('title');
-			obj.should.have.property('body');
-			done();
+	it("return a post if exist and comment if exist", function(done) {
+		request(url + '/blog/post/' + blogPostTitleToCreate, function(err, resp, body) {
+			assert.equal(resp.statusCode, 200)
+			var obj = JSON.parse(body)
+			obj.should.be.a('object')
+			obj.should.have.property('post')
+			obj.should.have.property('comments')
+			done()
 		});
 	});
 
@@ -96,8 +123,8 @@ describe('show a post', function() {
 
 
 
-var blogPostTitleToUpdate = 'new title';
-var blogPostMessageToUpdate = 'new simple message body';
+var blogPostTitleToUpdate = 'new title'
+var blogPostMessageToUpdate = 'new simple message body'
 
 describe('update a blog post', function() {
 	it('should return updated blog post', function(done) {
@@ -162,46 +189,16 @@ describe('delete a blog post', function() {
 	})
 })
 
-var blogPostIdToDeleteDoesNotExist = 30;
 describe('delete a non-existing blog post', function() {
 	it('should return 410', function(done) {
 		request({
 			method: 'DELETE',
-			url: url + '/blog/post/' + blogPostIdToDeleteDoesNotExist,
+			url: url + '/blog/post/' + 'blogPostIdToDeleteDoesNotExist',
 		}, function(err, res, body) {
 			if(err) {
 				done(err)
 			} else {
 				res.statusCode.should.be.equal(410)
-				done()
-			}
-		})
-	})
-})
-
-var commentAuthor = 'Andrei'
-var commentBody = 'Will it scale?'
-
-describe('create a comment for a blog post', function() {
-	it('should return created comment', function(done) {
-		request({
-			method: 'POST',
-			url: url + '/blog/comment/create/' + blogPostTitleToCreate,
-			json: true,
-			body: {
-				author: commentAuthor,
-				comment: commentBody
-			}
-		}, function(err, res, body) {
-			if(err) {
-				done(err)
-			} else {
-				res.statusCode.should.be.equal(200)
-				body.should.be.a('object')
-				body.should.have.property('author')
-				body.should.have.property('comment')
-				assert.equal(body.author, commentAuthor, 'returned author shall be the same as submitted')
-				assert.equal(body.comment, commentBody, 'returned message shall be the same as submitted')
 				done()
 			}
 		})
